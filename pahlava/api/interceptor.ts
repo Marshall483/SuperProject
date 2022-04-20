d85493f96befb877b97832bfee2bebf8af5e7bf0
Ильдар Гайнатуллин <ildar.gainatullin@simbirsoft.com>
2022-04-20 23:20:49 +0300

    fix: interceptor set up

--- pahlava/api/interceptor.ts
+++ pahlava/api/interceptor.ts
@@ -1,13 +1,12 @@
 import axios from "axios";
 import { getAuthToken } from "./cookieStorage";
 
-axios.interceptors.request.use((req) => {
-  req.headers = {
-    ...req.headers,
-    Authorization: getAuthToken(),
-  };
-  return req
-});
-
-
-
+export const setUpInterceptor = () => {
+  axios.interceptors.request.use((req) => {
+    req.headers = {
+      ...req.headers,
+      Authorization: getAuthToken(),
+    };
+    return req;
+  });
+};
--- pahlava/pages/_app.tsx
+++ pahlava/pages/_app.tsx
@@ -1,13 +1,18 @@
 import Head from "next/head";
+import { useEffect } from "react";
 import { AppProps } from "next/app";
 import { CssBaseline } from "@mui/material";
 import { ThemeProvider } from "@mui/material/styles";
 import { theme } from "../theme";
 import { Toaster } from "react-hot-toast";
-import '../api/interceptor'
+import { setUpInterceptor } from "../api/interceptor";
 
 const App = (props: AppProps) => {
   const { Component, pageProps } = props;
+
+  useEffect(() => {
+    setUpInterceptor();
+  }, []);
 
   return (
     <>
