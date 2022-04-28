9363e8c51e494003ab0a07a41fe9b0d61c8d9cf8
Ильдар Гайнатуллин <ildar.gainatullin@simbirsoft.com>
2022-04-28 21:03:10 +0300

    refactor: remove unused service

--- consumer/src/main/java/ru/itis/javalab/rabbit/RabbitOption.java
+++
@@ -1,7 +0,0 @@
-package ru.itis.javalab.rabbit;
-
-public abstract class RabbitOption {
-    public static final String EXCHANGE = "EXCHANGER";
-    public static final String QUEUE = "PDF_QUEUE";
-    public static final String ROUTING_KEY = "PDF_ROUTING_KEY";
-}
