b7cd5951c80fc13414d125b2d9f8276c497c4409
AzatNabiev <azat.nabiev-azat@yandex.ru>
2022-05-08 00:53:11 +0300

    added web client: Azat

---
+++ consumer-xlsx/src/main/java/ru/itis/javalab/models/WebClientModel.java
@@ -0,0 +1,22 @@
+package ru.itis.javalab.models;
+
+import lombok.*;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+@EqualsAndHashCode
+public class WebClientModel {
+    private UUID springId;
+    private UUID issueId;
+    private String issueName;
+    private String status;
+    private String projectName;
+    private double estimatedDueTimeInHours;
+    private double totalSpentTimeInHours;
+
+}
