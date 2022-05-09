7015b56cb46b347d47740d3aff13bec1d566d52e
AzatNabiev <azat.nabiev-azat@yandex.ru>
2022-05-09 20:18:22 +0300

    added cors, changed url

---
+++ consumer-xlsx/src/main/java/ru/itis/javalab/config/CorsConfig.java
@@ -0,0 +1,21 @@
+package ru.itis.javalab.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Slf4j
+@EnableWebMvc
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns(CorsConfiguration.ALL)
+                .allowedHeaders(CorsConfiguration.ALL)
+                .allowedMethods(CorsConfiguration.ALL);
+    }
+}
