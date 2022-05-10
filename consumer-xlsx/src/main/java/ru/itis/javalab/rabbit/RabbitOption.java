cbcbd5eb1b99c9178dac35fa53f43f1443e10b6a
AzatNabiev <azat.nabiev-azat@yandex.ru>
2022-05-10 23:04:29 +0300

    added rabbit config :azat

--- consumer-xlsx/src/main/java/ru/itis/javalab/rabbit/RabbitOption.java
+++ consumer-xlsx/src/main/java/ru/itis/javalab/rabbit/RabbitOption.java
@@ -2,4 +2,6 @@ package ru.itis.javalab.rabbit;
 
 public abstract class RabbitOption {
     public static final String EXCHANGE = "EXCHANGER";
+    public static final String QUEUE = "XLSX_QUEUE";
+    public static final String ROUTING_KEY = "XLSX_ROUTING_KEY";
 }
