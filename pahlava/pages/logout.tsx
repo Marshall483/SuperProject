7f169e5d291676b395ad13d145aa24ba21a241cc
Ильдар Гайнатуллин <ildar.gainatullin@simbirsoft.com>
2022-05-11 21:55:32 +0300

    feat: add logout route logic

---
+++ pahlava/pages/logout.tsx
@@ -0,0 +1,15 @@
+import { removeAuthToken } from "../api/cookieStorage";
+
+const Logout = () => <></>
+
+export const getServerSideProps = () => {
+  removeAuthToken();
+  return {
+    redirect: {
+      permanent: false,
+      destination: `/`,
+    },
+  };
+};
+
+export default Logout
