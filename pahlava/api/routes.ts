d5c23d33bdddd9abbdf27d19759ec0b2031f3505
Ильдар Гайнатуллин <ildar.gainatullin@simbirsoft.com>
2022-05-11 23:19:54 +0300

    feat: add all api routes

--- pahlava/api/routes.ts
+++ pahlava/api/routes.ts
@@ -3,12 +3,12 @@ export const userRegisterRoute = userRootRoute + "/user/register";
 export const userLoginRoute = userRootRoute + "/user/login";
 export const dataRootRoute = process.env.NEXT_PUBLIC_DATA_ROOT;
 export const newUserRegisterRoute =
-  dataRootRoute + "/api/Event/NewUserRegistration";
+  dataRootRoute + "/Event/NewUserRegistration";
 export const GetAllProjectsByUserIdRoute = (userId: string) =>
-  dataRootRoute + `/api/Projects/GetAllProjectsByUserId?UserId=${userId}`;
+  dataRootRoute + `/Projects/GetAllProjectsByUserId?UserId=${userId}`;
 export const postAllProjectsRoute =
-  dataRootRoute + `/api/Projects/AddOrUpdateProject`;
+  dataRootRoute + `/Projects/AddOrUpdateProject`;
 export const getReportBySprintIdRoute = (sprintId: string) =>
-  dataRootRoute + `/api/Report/GetReportBySprintId?sprintId=${sprintId}`;
+  dataRootRoute + `/Report/GetReportBySprintId?sprintId=${sprintId}`;
 export const getTasksBySprintIdRoute = (sprintId: string) =>
-  dataRootRoute + `/api/Task/GetTasksBySprintID?SprintId=${sprintId}`;
+  dataRootRoute + `/Task/GetTasksBySprintID?SprintId=${sprintId}`;
